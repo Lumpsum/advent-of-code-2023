666b9bb22266f3dbf41a2237223ef716ef7578f8
Lumpsum <vergunstje@hotmail.com>
2023-12-28 22:22:40 +0100

    Solve day 20

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,86 +1,33 @@
-use std::collections::{HashMap, VecDeque};
-
-use advent_of_code::repository::pulse::{ProcessPulse, Broadcaster, FlipFlop, FlipFlopState, Conjuction, Pulse};
+use advent_of_code::repository::pulse::{get_modules_and_edges, get_pulses, get_first_pulse, lcm_vec};
 use itertools::Itertools;
 
 advent_of_code::solution!(20);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut modules: HashMap<&str, Box<dyn ProcessPulse>> = HashMap::new();
-    let mut lines = input.lines().collect_vec();
-    lines.sort();
-    let mut edges = Vec::new();
-    for line in lines.into_iter().rev() {
-        let mut split = line.split(" -> ");
-        let module = split.nth(0).unwrap();
-        let destinations = split.nth(0).unwrap().split(", ").collect_vec();
-
-        match module {
-            "broadcaster" => modules.insert("broadcaster", Box::new(Broadcaster{ destinations: destinations })),
-            _ => {
-                let mut value = module.chars();
-                let first_value = value.next().unwrap();
-
-                match first_value {
-                    '%' => {
-                        for d in destinations.clone() {
-                            edges.push((module.rsplit_once("%").unwrap().1, d));
-                        };
-                        modules.insert(module.rsplit_once("%").unwrap().1, Box::new(FlipFlop{ state: FlipFlopState::Off, destinations: destinations }))
-                    },
-                    '&' => {
-                        for d in destinations.clone() {
-                            edges.push((module.rsplit_once("&").unwrap().1, d));
-                        };
-                        modules.insert(module.rsplit_once("&").unwrap().1, Box::new(Conjuction{ inputs: HashMap::new(), destinations: destinations }))
-                    },
-                    _ => continue
-                }
-            }
-        };
-    };
-
-    for edge in edges.into_iter() {
-        if let Some(v) = modules.get_mut(edge.1) {
-            if let Some(v) = v.get_inputs() {
-                v.insert(edge.0, Pulse::Low);
-            }
-        }
-    }
-
-    let mut low_pulses = 0;
-    let mut high_pulses = 0;
-
-    for _ in 0..1000 {
-        let mut q = VecDeque::new();
-        q.push_front(("button", Pulse::Low, "broadcaster"));
-
-        while let Some((sender, pulse, key)) = q.pop_front() {
-            match pulse {
-                Pulse::High => high_pulses += 1,
-                Pulse::Low => low_pulses += 1,
-            }
-
-            let module = match modules.get_mut(key) {
-                Some(v) => v,
-                None => continue
-            };
-            match module.process_pulse(pulse, sender) {
-                Some(v) => {
-                    for d in v.into_iter() {
-                        q.push_back((key, d.1, d.0))
-                    }
-                },
-                None => ()
-            }
-        }
-    }
-
-    Some(low_pulses * high_pulses)
+pub fn part_one<'a>(input: &'a str) -> Option<u32> {
+    let (modules, _) = get_modules_and_edges(input);
+    Some(get_pulses(1000, modules))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (mut modules, edges) = get_modules_and_edges(input);
+
+    let mut input: &str = "";
+    for edge in edges {
+        if edge.1 == "rx" {
+            input = edge.0;
+            break
+        }
+    }
+
+    let inputs = modules.get_mut(input).unwrap().get_inputs().unwrap();
+    let first_pulses = get_first_pulse(inputs.keys().map(|&s| s).collect_vec(), modules);
+    
+    // println!("{:?}", inputs.keys().collect_vec());
+    // {"rk": Low, "zf": Low, "qx": Low, "cd": Low}
+
+    // press_button(15000, modules);
+
+    Some(lcm_vec(first_pulses))
 }
 
 #[cfg(test)]
@@ -96,6 +43,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(0));
     }
 }
--- src/repository/pulse.rs
+++ src/repository/pulse.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 use itertools::Itertools;
+use num::{integer::lcm, Integer};
 
 #[derive(PartialEq, Eq, Debug)]
 pub enum Pulse {
@@ -84,4 +85,141 @@ impl<'a> ProcessPulse<'a> for Conjuction<'a> {
     fn get_destinations(&self) -> &Vec<&'a str> {
         &self.destinations
     }
+}
+
+
+pub fn get_modules_and_edges<'a>(s: &'a str) -> (HashMap<&'a str, Box<dyn ProcessPulse + 'a>>, Vec<(&'a str, &'a str)>) {
+    let mut modules: HashMap<&str, Box<dyn ProcessPulse + 'a>> = HashMap::new();
+    let mut lines = s.lines().collect_vec();
+    lines.sort();
+    let mut edges = Vec::new();
+    for line in lines.into_iter().rev() {
+        let mut split = line.split(" -> ");
+        let module = split.nth(0).unwrap();
+        let destinations = split.nth(0).unwrap().split(", ").collect_vec();
+
+        match module {
+            "broadcaster" => modules.insert("broadcaster", Box::new(Broadcaster{ destinations: destinations })),
+            _ => {
+                let mut value = module.chars();
+                let first_value = value.next().unwrap();
+
+                match first_value {
+                    '%' => {
+                        for d in destinations.clone() {
+                            edges.push((module.rsplit_once("%").unwrap().1, d));
+                        };
+                        modules.insert(module.rsplit_once("%").unwrap().1, Box::new(FlipFlop{ state: FlipFlopState::Off, destinations: destinations }))
+                    },
+                    '&' => {
+                        for d in destinations.clone() {
+                            edges.push((module.rsplit_once("&").unwrap().1, d));
+                        };
+                        modules.insert(module.rsplit_once("&").unwrap().1, Box::new(Conjuction{ inputs: HashMap::new(), destinations: destinations }))
+                    },
+                    _ => continue
+                }
+            }
+        };
+    };
+
+    for edge in edges.iter() {
+        if let Some(v) = modules.get_mut(edge.1) {
+            if let Some(v) = v.get_inputs() {
+                v.insert(edge.0, Pulse::Low);
+            }
+        }
+    }
+
+    (modules, edges)
+}
+
+
+pub fn get_pulses<'a>(n: usize, mut modules: HashMap<&str, std::boxed::Box<dyn ProcessPulse + 'a>>) -> u32 {
+    let mut low_pulses = 0;
+    let mut high_pulses = 0;
+
+    for i in 0..n {
+        let mut q = VecDeque::new();
+        q.push_front(("button", Pulse::Low, "broadcaster"));
+
+        while let Some((sender, pulse, key)) = q.pop_front() {
+            match pulse {
+                Pulse::High => high_pulses += 1,
+                Pulse::Low => low_pulses += 1,
+            }
+
+            if sender == "zf" && pulse == Pulse::High {
+                println!("{:?}", i);
+                panic!("")
+            }
+
+            let module = match modules.get_mut(key) {
+                Some(v) => v,
+                None => continue
+            };
+            match module.process_pulse(pulse, sender) {
+                Some(v) => {
+                    for d in v.into_iter() {
+                        q.push_back((key, d.1, d.0))
+                    }
+                },
+                None => ()
+            }
+        }
+    }
+
+    low_pulses * high_pulses
+}
+
+
+pub fn get_first_pulse<'a>(module_names: Vec<&str>, mut modules: HashMap<&str, std::boxed::Box<dyn ProcessPulse + 'a>>) -> Vec<u64> {
+    let mut result = vec![0; module_names.len()];
+
+    let mut i = 0;
+
+    loop {
+        i += 1;
+        let mut q = VecDeque::new();
+        q.push_front(("button", Pulse::Low, "broadcaster"));
+
+        while let Some((sender, pulse, key)) = q.pop_front() {
+            if pulse == Pulse::High && module_names.contains(&sender) {
+                let index = module_names.iter().position(|&s| s == sender).unwrap();
+                if result[index] == 0 {
+                    result[index] = i
+                }
+            }
+
+            let module = match modules.get_mut(key) {
+                Some(v) => v,
+                None => continue
+            };
+            match module.process_pulse(pulse, sender) {
+                Some(v) => {
+                    for d in v.into_iter() {
+                        q.push_back((key, d.1, d.0))
+                    }
+                },
+                None => ()
+            }
+        }
+
+        if !result.iter().any(|&s| s == 0) {
+            break;
+        }
+    }
+    
+    result
+}
+
+
+pub fn lcm_vec<T>(numbers: Vec<T>) -> T 
+where T: Integer + Copy + Clone {
+    let mut numbers_iter = numbers.iter();
+    let mut current_number = *numbers_iter.next().unwrap();
+    while let Some(next_number) = numbers_iter.next() {
+        current_number = lcm(current_number, *next_number)
+    }
+    current_number
 }
